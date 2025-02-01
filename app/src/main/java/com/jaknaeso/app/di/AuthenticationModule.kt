43540fbe9d35e4eb44a4b3a342004ebd4402fa31
이용인 <yongin0517@gmail.com>
2025-02-02 01:44:46 +0900

    Feat: hilt 객체 주입 모듈 생성

---
+++ app/src/main/java/com/jaknaeso/app/di/AuthenticationModule.kt
@@ -0,0 +1,30 @@
+package com.jaknaeso.app.di
+
+import com.jaknaeso.app.data.authentication.Authenticator
+import com.jaknaeso.app.data.authentication.AuthenticatorImpl
+import com.jaknaeso.app.data.authentication.RefreshTokenManager
+import com.jaknaeso.app.data.authentication.RefreshTokenManagerImpl
+import com.jaknaeso.app.data.token.TokenManager
+import com.jaknaeso.app.data.token.TokenManagerImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface AuthenticationModule {
+
+    @Binds
+    @Singleton
+    fun bindTokenManager(tokenManagerImpl: TokenManagerImpl): TokenManager
+
+    @Binds
+    @Singleton
+    fun bindRefreshTokenManager(refreshTokenManagerImpl: RefreshTokenManagerImpl): RefreshTokenManager
+
+    @Binds
+    @Singleton
+    fun bindAuthenticator(authenticatorImpl: AuthenticatorImpl): Authenticator
+}
--- app/src/main/java/com/jaknaeso/app/di/DatastoreModule.kt
+++ app/src/main/java/com/jaknaeso/app/di/DatastoreModule.kt
@@ -1,4 +1,17 @@
 package com.jaknaeso.app.di
 
-object DatastoreModule {
+import com.jaknaeso.app.data.datastore.LoginRemoteDatastore
+import com.jaknaeso.app.data.datastoreImpl.LoginRemoteDatastoreImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface DatastoreModule {
+    @Singleton
+    @Binds
+    fun provideLoginDatastore(loginDatastoreImpl: LoginRemoteDatastoreImpl): LoginRemoteDatastore
 }
