da839e3e7d0019e82e45547b09907454f50a2ef7
이용인 <yongin0517@gmail.com>
2025-02-01 19:29:27 +0900

    Chore: 마니페스트 설정 변경

--- app/src/main/java/com/jaknaeso/app/Application.kt
+++ app/src/main/java/com/jaknaeso/app/Application.kt
@@ -2,7 +2,9 @@ package com.jaknaeso.app
 
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class Application : Application() {
     override fun onCreate() {
         super.onCreate()
