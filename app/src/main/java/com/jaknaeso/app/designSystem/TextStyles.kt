f85e19804a0fc11664d2872ae1a75d9ba38158db
이용인 <yongin0517@gmail.com>
2025-02-01 20:56:27 +0900

    Design: 텍스트,색상, 폰트 추가

---
+++ app/src/main/java/com/jaknaeso/app/designSystem/TextStyles.kt
@@ -0,0 +1,20 @@
+package com.jaknaeso.app.designSystem
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+object TextStyles {
+    val title01 = TextStyle(fontWeight = FontWeight.Bold, fontSize = 32.sp)
+    val title02 = TextStyle(fontWeight = FontWeight.Bold, fontSize = 24.sp)
+    val title03 = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
+    val title04 = TextStyle(fontWeight = FontWeight.Bold, fontSize = 18.sp)
+    val subTitle01 = TextStyle(fontWeight = FontWeight.SemiBold, fontSize = 18.sp)
+    val subTitle02 = TextStyle(fontWeight = FontWeight.Normal, fontSize = 18.sp)
+    val subTitle03 = TextStyle(fontWeight = FontWeight.SemiBold, fontSize = 16.sp)
+    val subTitle04 = TextStyle(fontWeight = FontWeight.Normal, fontSize = 16.sp)
+    val subTitle05 = TextStyle(fontWeight = FontWeight.SemiBold, fontSize = 14.sp)
+    val body01 = TextStyle(fontWeight = FontWeight.Medium, fontSize = 14.sp)
+    val body02 = TextStyle(fontWeight = FontWeight.Medium, fontSize = 12.sp)
+    val caption = TextStyle(fontWeight = FontWeight.Medium, fontSize = 10.sp)
+}
