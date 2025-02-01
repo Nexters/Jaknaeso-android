7e830811ef4b3098e33c6cd0c4a872293bcec4d2
이용인 <yongin0517@gmail.com>
2025-02-01 13:20:10 +0900

    Rename

--- app/src/main/java/com/jaknaeso/app/presentation/NavHost.kt
+++ app/src/main/java/com/jaknaeso/app/presentation/NavHost.kt
@@ -1,8 +1,13 @@
 package com.jaknaeso.app.presentation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import com.jaknaeso.app.presentation.login.loginScreen
+
+fun NavController.navigateToHome() = navigate("${Route.Home}")
+fun NavController.navigateToLogin() = navigate("${Route.Login}")
 
 @Composable
 fun SetUpNavGraph(
@@ -13,6 +18,6 @@ fun SetUpNavGraph(
         navController = navController,
         startDestination = startDestination
     ) {
-
+        loginScreen(navigateToHome = { navController.navigateToHome() })
     }
 }
