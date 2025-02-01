b22d3f2dad0676ac264fe688df80749d1fd423a2
이용인 <yongin0517@gmail.com>
2025-02-01 20:56:48 +0900

    Design: TextStyles 객체 생성

--- app/src/main/java/com/jaknaeso/app/designSystem/TextStyles.kt
+++ app/src/main/java/com/jaknaeso/app/designSystem/TextStyles.kt
@@ -1,20 +1,28 @@
 package com.jaknaeso.app.designSystem
 
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.jaknaeso.app.R
 
 object TextStyles {
-    val title01 = TextStyle(fontWeight = FontWeight.Bold, fontSize = 32.sp)
-    val title02 = TextStyle(fontWeight = FontWeight.Bold, fontSize = 24.sp)
-    val title03 = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
-    val title04 = TextStyle(fontWeight = FontWeight.Bold, fontSize = 18.sp)
-    val subTitle01 = TextStyle(fontWeight = FontWeight.SemiBold, fontSize = 18.sp)
-    val subTitle02 = TextStyle(fontWeight = FontWeight.Normal, fontSize = 18.sp)
-    val subTitle03 = TextStyle(fontWeight = FontWeight.SemiBold, fontSize = 16.sp)
-    val subTitle04 = TextStyle(fontWeight = FontWeight.Normal, fontSize = 16.sp)
-    val subTitle05 = TextStyle(fontWeight = FontWeight.SemiBold, fontSize = 14.sp)
-    val body01 = TextStyle(fontWeight = FontWeight.Medium, fontSize = 14.sp)
-    val body02 = TextStyle(fontWeight = FontWeight.Medium, fontSize = 12.sp)
-    val caption = TextStyle(fontWeight = FontWeight.Medium, fontSize = 10.sp)
+    val bold = FontFamily(Font(R.font.pretendard_bold))
+    val semiBold = FontFamily(Font(R.font.pretendard_semi_bold))
+    val medium = FontFamily(Font(R.font.pretendard_medium))
+    val regular = FontFamily(Font(R.font.pretendard_regular))
+
+    val title01 = TextStyle(fontFamily = bold, fontSize = 32.sp)
+    val title02 = TextStyle(fontFamily = bold, fontSize = 24.sp)
+    val title03 = TextStyle(fontFamily = bold, fontSize = 20.sp)
+    val title04 = TextStyle(fontFamily = bold, fontSize = 18.sp)
+    val subTitle01 = TextStyle(fontFamily = semiBold, fontSize = 18.sp)
+    val subTitle02 = TextStyle(fontFamily = regular, fontSize = 18.sp)
+    val subTitle03 = TextStyle(fontFamily = semiBold, fontSize = 16.sp)
+    val subTitle04 = TextStyle(fontFamily = regular, fontSize = 16.sp)
+    val subTitle05 = TextStyle(fontFamily = semiBold, fontSize = 14.sp)
+    val body01 = TextStyle(fontFamily = medium, fontSize = 14.sp)
+    val body02 = TextStyle(fontFamily = medium, fontSize = 12.sp)
+    val caption = TextStyle(fontFamily = medium, fontSize = 10.sp)
 }
