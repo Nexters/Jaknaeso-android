af40293f2161652545d2fe4f1ed38e57b9c5be77
이용인 <yongin0517@gmail.com>
2025-02-03 20:40:32 +0900

    Fix: 개발목적의 임시 라우팅 변경

--- app/src/main/java/com/jaknaeso/app/presentation/main/MainActivity.kt
+++ app/src/main/java/com/jaknaeso/app/presentation/main/MainActivity.kt
@@ -2,11 +2,10 @@ package com.jaknaeso.app.presentation.main
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.rememberNavController
+import com.jaknaeso.app.presentation.navigation.Route
 import com.jaknaeso.app.presentation.navigation.SetUpNavGraph
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,16 +13,12 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-
         setContent {
             val viewmodel: MainViewmodel = hiltViewModel()
             val navHostController = rememberNavController()
-            val uiState = viewmodel.uiState.collectAsStateWithLifecycle()
-
             SetUpNavGraph(
                 navController = navHostController,
-                startDestination = uiState.value.initialRoute,
+                startDestination = Route.Home.name,
             )
         }
     }
