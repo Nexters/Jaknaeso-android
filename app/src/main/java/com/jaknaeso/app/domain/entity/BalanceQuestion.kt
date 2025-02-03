04648f19fa630a86882f15c45f14d6bdc6ed477b
이용인 <yongin0517@gmail.com>
2025-02-04 04:58:24 +0900

    Rename: 이중선택 뜻에 맞는 entitity 이름 변경

--- app/src/main/java/com/jaknaeso/app/domain/entity/BalanceQuestion.kt
+++ app/src/main/java/com/jaknaeso/app/domain/entity/BalanceQuestion.kt
@@ -1,6 +1,7 @@
 package com.jaknaeso.app.domain.entity
 
-data class VersusQuestion(
+data class BalanceQuestion(
+    val id: Int,
     val roundIndex: Int,
     val question: String,
     val options: List<String>
