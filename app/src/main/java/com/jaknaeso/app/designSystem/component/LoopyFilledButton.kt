f390b578d7c6235acb0556d7eff6df0f4df30aca
이용인 <yongin0517@gmail.com>
2025-02-14 07:05:06 +0900

    Design: svg 아이콘 추가 및 UI 컴포넌트 수정

--- app/src/main/java/com/jaknaeso/app/designSystem/component/LoopyFilledButton.kt
+++ app/src/main/java/com/jaknaeso/app/designSystem/component/LoopyFilledButton.kt
@@ -1,5 +1,7 @@
 package com.jaknaeso.app.designSystem.component
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
@@ -13,6 +15,7 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.jaknaeso.app.R
 import com.jaknaeso.app.designSystem.theme.ColorPalette
@@ -21,20 +24,26 @@ import com.jaknaeso.app.designSystem.theme.TextStyles
 @Composable
 fun LoopyFilledButton(
     text: String,
-    icon: Painter? = null,
-    iconColor: Color = Color.White,
+    leadingIcon: Painter? = null,
+    trailingIcon: Painter? = null,
+    leadingIconColor: Color = Color.Black,
+    trailingIconColor: Color = Color.Black,
+    iconSize:Dp = 20.dp,
     onClick: () -> Unit,
+    height:Dp = 58.dp,
     modifier: Modifier,
     enabled: Boolean = true,
     filledColor: Color = ColorPalette.PrimaryBlue500,
     disabledColor: Color = ColorPalette.Neautral300,
     textColor: Color = Color.White,
-    textStyle: TextStyle = TextStyles.subTitle01
+    textStyle: TextStyle = TextStyles.subTitle01,
+    borderColor: Color? = null
 ) {
-    val buttonModifier = Modifier.then(modifier ?: Modifier)
+    val borderStroke = BorderStroke(width = 1.4.dp, color = if (borderColor != null) borderColor else filledColor)
+    val buttonModifier = Modifier.then(modifier)
     FilledIconButton(
         onClick = onClick,
-        modifier = buttonModifier.height(58.dp),
+        modifier = buttonModifier.height(height).border(borderStroke, shape = RoundedCornerShape(12.dp)),
         colors = IconButtonColors(
             containerColor = filledColor,
             contentColor = filledColor,
@@ -44,16 +53,24 @@ fun LoopyFilledButton(
         enabled = enabled,
         shape = RoundedCornerShape(12.dp)
     ) {
-        Row {
-            if (icon != null) {
+        Row(horizontalArrangement = Arrangement.Center) {
+            if (leadingIcon != null) {
                 Icon(
-                    painter = icon,
-                    tint = iconColor,
+                    painter = leadingIcon,
+                    tint = leadingIconColor,
                     contentDescription = null,
-                    modifier = Modifier.padding(end = 8.dp)
+                    modifier = Modifier.padding(end = 8.dp).size(iconSize)
                 )
             }
             Text("${text}", color = textColor, style = textStyle)
+            if (trailingIcon != null) {
+                Icon(
+                    painter = trailingIcon,
+                    tint = trailingIconColor,
+                    contentDescription = null,
+                    modifier = Modifier.padding(start = 8.dp).size(iconSize)
+                )
+            }
         }
     }
 }
@@ -65,8 +82,8 @@ fun ButtonPreview() {
     Column(modifier = Modifier.fillMaxSize()) {
         LoopyFilledButton(
             "로그인하기",
-            icon = painterResource(R.drawable.ic_kakao),
-            iconColor = Color.Black,
+            leadingIcon = painterResource(R.drawable.ic_kakao),
+            leadingIconColor = Color.Black,
             onClick = {},
             modifier = Modifier.fillMaxWidth(1f),
         )
@@ -81,7 +98,11 @@ fun ButtonPreview() {
                 onClick = {},
                 modifier = Modifier.fillMaxWidth(1f).padding(horizontal = 4.dp),
                 filledColor = ColorPalette.Neautral200,
-                textColor = ColorPalette.Neautral600
+                textColor = ColorPalette.Neautral600,
+                borderColor = ColorPalette.Neautral800,
+                trailingIcon = painterResource(R.drawable.ic_kakao),
+                trailingIconColor = ColorPalette.Neautral600,
+                height = 50.dp
             )
         }
     }
--- app/src/main/java/com/jaknaeso/app/designSystem/component/LoopyRadioButtons.kt
+++
@@ -1,2 +0,0 @@
-package com.jaknaeso.app.designSystem.component
-
