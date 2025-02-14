a24adaad2a9382e01631dd6ebc094566cf5b0a29
이용인 <yongin0517@gmail.com>
2025-02-15 05:58:25 +0900

    Feat: 전체라운드 및 개별라운드 조회 유스케이스 로직 수정

--- app/src/main/java/com/jaknaeso/app/domain/usecase/GetBalanceQuestionUseCase.kt
+++ app/src/main/java/com/jaknaeso/app/domain/usecase/GetBalanceQuestionUseCase.kt
@@ -1,19 +1,42 @@
 package com.jaknaeso.app.domain.usecase
 
-import com.jaknaeso.app.domain.repository.BalanceQuestionRepository
+import com.jaknaeso.app.domain.entity.ResponseResult
+import com.jaknaeso.app.domain.model.Option
+import com.jaknaeso.app.domain.model.RoundQuestion
+import com.jaknaeso.app.domain.model.SurveyType
+import com.jaknaeso.app.domain.repository.SurveyRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
-class GetBalanceQuestionUseCase @Inject constructor(private val balanceQuestionRepository: BalanceQuestionRepository) {
-    suspend operator fun invoke(roundIndex: Int): com.jaknaeso.app.domain.model.BalanceQuestion? {
-        val result = balanceQuestionRepository.getQuestionByRoundIndex(roundIndex)
-        if (result != null) {
-            return com.jaknaeso.app.domain.model.BalanceQuestion(
-                id = result.id,
-                roundIndex = result.roundIndex,
-                question = result.question,
-                options = result.options
-            )
+class GetBalanceQuestionUseCase @Inject constructor(
+    private val surveyRepository: SurveyRepository
+) {
+
+    suspend operator fun invoke(bundleId: String): Flow<RoundQuestion?> {
+        val data = surveyRepository.getSurvey(bundleId)
+        return flow {
+            if (data.result == ResponseResult.SUCCESS.name) {
+                val result = RoundQuestion(
+                    id = data.data?.id.toString(),
+                    surveyType = data.data?.surveyType.mapToSurveyType(),
+                    content = data.data?.contents ?: "",
+                    options = data.data?.options?.map {
+                        Option(it.id.toString(), it.optionContents)
+                    } ?: emptyList()
+                )
+                emit(result)
+            } else {
+                throw Exception(data.error?.message)
+            }
         }
-        return null
+    }
+
+    fun String?.mapToSurveyType(): SurveyType {
+        if (this == SurveyType.BALANCE.name) {
+            return SurveyType.BALANCE
+        } else {
+            return SurveyType.MULTIPLE_CHOICE
+        }
     }
 }
--- app/src/main/java/com/jaknaeso/app/domain/usecase/GetRoundsUseCase.kt
+++ app/src/main/java/com/jaknaeso/app/domain/usecase/GetRoundsUseCase.kt
@@ -4,53 +4,44 @@ import com.jaknaeso.app.domain.entity.ResponseResult
 import com.jaknaeso.app.domain.model.QuestionState
 import com.jaknaeso.app.domain.model.Round
 import com.jaknaeso.app.domain.model.RoundBundle
-import com.jaknaeso.app.domain.repository.RoundRepository
 import com.jaknaeso.app.domain.repository.SurveyRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GetRoundsUseCase @Inject constructor(
-    private val roundRepository: RoundRepository,
     private val surveyRepository: SurveyRepository
 ) {
     val ROUNDS = 15
-    suspend operator fun invoke(): Flow<RoundBundle?> {
+    suspend operator fun invoke(): Flow<RoundBundle> {
         val data = surveyRepository.getSurveysHistory()
-        var result: RoundBundle? = null
         val rounds = mutableListOf<Round>()
-        if (data.result == ResponseResult.SUCCESS.name) {
+        return flow {
+            if (data.result == ResponseResult.SUCCESS.name) {
 
-            data.data?.surveyHistoryDetails?.map {
-                rounds.add(Round(it.submissionId, QuestionState.PAST))
-            }
+                data.data?.surveyHistoryDetails?.map {
+                    rounds.add(Round(it.submissionId, QuestionState.PAST))
+                }
 
-            if (data.data?.isCompleted!!) {
-                rounds.add(Round(roundId = data.data?.nextSurveyIndex!!, state = QuestionState.TODAY_COMPLETED))
-            } else {
-                rounds.add(Round(roundId = data.data?.nextSurveyIndex!!, state = QuestionState.TODAY_LOCKED))
-            }
+                if (data.data?.isCompleted!!) {
+                    rounds.add(Round(roundId = data.data?.nextSurveyIndex!!, state = QuestionState.TODAY_COMPLETED))
+                } else {
+                    rounds.add(Round(roundId = data.data?.nextSurveyIndex!!, state = QuestionState.TODAY_LOCKED))
+                }
 
-            for (i in rounds.size until ROUNDS) {
-                rounds.add(Round(roundId = null, state = QuestionState.FUTURE))
-            }
-            result = RoundBundle(
-                bundleId = data.data?.bundleId,
-                rounds = rounds,
-                isTodayRoundCompleted = data.data?.isCompleted ?: false
-            )
-        }
-        return flow { emit(result) }
-    }
-
-    suspend fun getDummyData(): Flow<List<Round>> {
-        return roundRepository.getAllRounds().map {
-            it.map {
-                Round(
-                    roundId = it.roundIndex,
-                    state = QuestionState.PAST
+                for (i in rounds.size until ROUNDS) {
+                    rounds.add(Round(roundId = null, state = QuestionState.FUTURE))
+                }
+                emit(
+                    RoundBundle(
+                        bundleId = data.data?.bundleId,
+                        rounds = rounds,
+                        isTodayRoundCompleted = data.data?.isCompleted ?: false
+                    )
                 )
+
+            } else {
+                throw Exception(data.error?.message)
             }
         }
     }
