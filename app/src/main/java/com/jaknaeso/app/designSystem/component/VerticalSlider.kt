bdebb2f5eea779ea5aa53e17987f1751a8e770a0
이용인 <yongin0517@gmail.com>
2025-02-14 14:53:58 +0900

    Feat: 슬라이더 선택화면 생성

---
+++ app/src/main/java/com/jaknaeso/app/designSystem/component/VerticalSlider.kt
@@ -0,0 +1,214 @@
+package com.jaknaeso.app.designSystem.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.dp
+import com.jaknaeso.app.R
+import com.jaknaeso.app.designSystem.theme.ColorPalette
+import com.jaknaeso.app.designSystem.theme.TextStyles
+
+@Composable
+fun VerticalSliderForm(answerList: List<String>, onValueChange: (index:Int) -> Unit) {
+    var value by remember { mutableStateOf(0.5f) }
+    var isSelected by remember { mutableStateOf(2) }
+    val HEIGHT = 278.dp
+
+    LaunchedEffect(value) {
+        if (value in 0f..0.2f) {
+            isSelected = 4
+        } else if (value in 0.21f..0.4f) {
+            isSelected = 3
+        } else if (value in 0.41f..0.6f) {
+            isSelected = 2
+        } else if (value in 0.61f..0.8f) {
+            isSelected = 1
+        } else {
+            isSelected = 0
+        }
+    }
+
+    LaunchedEffect(isSelected){
+        onValueChange(4-isSelected)
+    }
+
+    Row(
+        modifier = Modifier.height(HEIGHT).fillMaxWidth().background(color = Color.Transparent),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        LazyColumn(modifier = Modifier.padding(vertical = 12.dp).fillMaxWidth(0.9f).height(HEIGHT).background(color = Color.Transparent), verticalArrangement = Arrangement.SpaceBetween, horizontalAlignment = Alignment.Start) {
+            itemsIndexed(items = answerList) { index, item ->
+                SliderAnswer(isSelected = index == isSelected, answer = item)
+            }
+        }
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxHeight(1f).background(color = Color.Transparent)
+        ) {
+            VerticalSlider(
+                value = value,
+                onValueChange = {
+                    value = it
+                },
+                valueRanges = listOf(0f..0.2f, 0.21f..0.4f, 041f..0.6f, 061f..0.8f, 0.81f..1f), // 특정 구간 자동 설정
+                modifier = Modifier.fillMaxHeight(1f).background(color = Color.Transparent)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+    }
+}
+
+@Composable
+fun SliderAnswer(isSelected: Boolean, answer: String) {
+    if (!isSelected) {
+        Text(answer, style = TextStyles.subTitle02, color = ColorPalette.Neautral600)
+    } else {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                painter = painterResource(R.drawable.ic_check),
+                contentDescription = null,
+                tint = Color.White,
+                modifier = Modifier.background(color = ColorPalette.PrimaryBlue500, shape = CircleShape).size(26.dp).padding(5.dp)
+            )
+            Text(
+                answer,
+                style = TextStyles.subTitle01,
+                color = Color.Black,
+                modifier = Modifier.padding(start = 5.5.dp)
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun VerticalSlider(
+    value: Float,
+    onValueChange: (Float) -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    valueRanges: List<ClosedFloatingPointRange<Float>> = listOf(0f..1f),
+    steps: Int = 0,
+    onValueChangeFinished: (() -> Unit)? = null,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
+) {
+    var adjustedValue by remember { mutableStateOf(value) }
+
+    Slider(
+        colors = SliderDefaults.colors(
+            thumbColor = ColorPalette.PrimaryBlue500,
+            activeTrackColor = ColorPalette.PrimaryBlue500,
+            inactiveTrackColor = ColorPalette.PrimaryBlue500
+        ),
+        interactionSource = interactionSource,
+        onValueChangeFinished = {
+            val closestRange = valueRanges.minByOrNull { range ->
+                val center = (range.start + range.endInclusive) / 2
+                kotlin.math.abs(adjustedValue - center)
+            }
+
+            closestRange?.let { range ->
+                adjustedValue = when {
+                    range.start == 0f && range.endInclusive <= 0.2f -> 0f
+                    range.start >= 0.81f && range.endInclusive == 1f -> 1f
+                    else -> (range.start + range.endInclusive) / 2
+                }
+                onValueChange(adjustedValue)
+            }
+            onValueChangeFinished?.invoke()
+        },
+        steps = steps,
+        value = adjustedValue,
+        valueRange = valueRanges.first().start..valueRanges.last().endInclusive,
+        enabled = enabled,
+        onValueChange = { newValue ->
+            val validRange = valueRanges.find { newValue in it }
+            if (validRange != null) {
+                adjustedValue = newValue
+                onValueChange(newValue)
+            }
+        },
+        modifier = Modifier
+            .graphicsLayer {
+                rotationZ = 270f
+                transformOrigin = TransformOrigin(0f, 0f)
+            }
+            .layout { measurable, constraints ->
+                val placeable = measurable.measure(
+                    Constraints(
+                        minWidth = constraints.minHeight,
+                        maxWidth = constraints.maxHeight,
+                        minHeight = 100,
+                        maxHeight = 100,
+                    )
+                )
+                layout(placeable.height, placeable.width) {
+                    placeable.place(-placeable.width, 0)
+                }
+            }
+            .then(modifier),
+        track = {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(8.dp)
+                    .background(ColorPalette.PrimaryBlue500, shape = RoundedCornerShape(50)) // 둥글게 처리
+            )
+        },
+        thumb = {
+            Box(
+                modifier = Modifier
+                    .size(26.dp) // Thumb 크기 조정
+                    .background(ColorPalette.PrimaryBlue500, shape = CircleShape)
+                    .border(2.dp, ColorPalette.PrimaryBlue500, shape = CircleShape),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_arrow_vertical_side),
+                    contentDescription = "Thumb Icon",
+                    tint = Color.White, // 아이콘 색상 설정
+                    modifier = Modifier.size(24.dp) // 아이콘 크기 조정
+                )
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+fun SliderPreview() {
+    var value by remember { mutableStateOf(0.5f) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xffdedede)),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        VerticalSliderForm(answerList = listOf("매우 동의해요","조금 동의해요","보통이에요","조금 반대해요","매우 반대해요"),{})
+    }
+}
---
+++ app/src/main/java/com/jaknaeso/app/presentation/view/SliderRoundScreen.kt
@@ -0,0 +1,139 @@
+package com.jaknaeso.app.presentation.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.jaknaeso.app.R
+import com.jaknaeso.app.designSystem.component.*
+import com.jaknaeso.app.designSystem.theme.ColorPalette
+import com.jaknaeso.app.designSystem.theme.TextStyles
+import com.jaknaeso.app.presentation.contract.BalanceRoundEvent
+import com.jaknaeso.app.presentation.viewmodel.BalanceRoundViewmodel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SliderRoundScreen(
+    navigateToBalanceRoundComplete: () -> Unit,
+    navigateToBack: () -> Unit,
+    roundIndex: String,
+    viewmodel: BalanceRoundViewmodel = hiltViewModel()
+) {
+    var isModalExpanded by remember { mutableStateOf(false) }
+    val bottomSheetState = androidx.compose.material3.rememberModalBottomSheetState(skipPartiallyExpanded = true)
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize().background(color = ColorPalette.Neautral0)
+    ) { paddingValue ->
+        if (isModalExpanded) {
+            ModalBottomSheet(
+                onDismissRequest = { isModalExpanded = false },
+                modifier = Modifier.background(color = Color.Transparent),
+                sheetState = bottomSheetState,
+                contentColor = Color.White,
+                dragHandle = { DragHandle(onClick = {}) }
+            ) {
+                Column(
+                    modifier = Modifier.background(color = Color.White).padding(horizontal = 20.dp)
+                        .fillMaxWidth(1f),
+                    horizontalAlignment = Alignment.Start,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Spacer(modifier = Modifier.fillMaxWidth().height(8.dp))
+                    Text(text = "답변을 선택한 이유를 알려주세요", style = TextStyles.title03)
+                    Spacer(modifier = Modifier.fillMaxWidth().height(20.dp))
+                    LoopyTextField(
+                        placeHolderValue = "오늘의 나에게 집중해서 적어보세요",
+                        onValueChange = {},
+                        modifier = Modifier.fillMaxWidth(1f)
+                    )
+                    Spacer(modifier = Modifier.fillMaxWidth().height(32.dp))
+                    Row(horizontalArrangement = Arrangement.SpaceBetween) {
+                        LoopyFilledButton(
+                            "작성 완료",
+                            onClick = { viewmodel.handleEvent(BalanceRoundEvent.ClickSubmitReasonButton) },
+                            modifier = Modifier.fillMaxWidth(0.5f).padding(horizontal = 4.dp)
+                        )
+                        LoopyFilledButton(
+                            "넘어가기",
+                            onClick = { viewmodel.handleEvent(BalanceRoundEvent.ClickBackButton) },
+                            modifier = Modifier.fillMaxWidth(1f).padding(horizontal = 4.dp),
+                            filledColor = ColorPalette.Neautral200,
+                            textColor = ColorPalette.Neautral600
+                        )
+                    }
+                    Spacer(modifier = Modifier.fillMaxWidth().height(62.dp))
+                }
+            }
+        }
+        Column(
+            Modifier.fillMaxSize(1f).padding(paddingValue).background(color = ColorPalette.Neautral0),
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+            LoopyTopBar(
+                title = "오늘의 질문",
+                icon = painterResource(R.drawable.ic_back),
+                onClickIcon = { viewmodel.handleEvent(BalanceRoundEvent.ClickBackButton) })
+            SliderContent(onClickFinishButton = {roundIndex ->  })
+        }
+    }
+}
+
+@Composable
+fun SliderContent(onClickFinishButton: (roundIndex:Int) -> Unit) {
+    var selectedIndex by remember { mutableStateOf(2) } //2가 VerticalSliderForm 보통 디폴트 값
+    Column(
+        Modifier.padding(horizontal = 20.dp).padding(top = 54.dp, bottom = 28.dp).fillMaxSize(1f),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(Modifier.padding(horizontal = 58.dp)) {
+            Text(
+                "독립에 대한 고민이 깊어지는 요즘... 드디어 결정을 내렸다.",
+                style = TextStyles.title03,
+                color = Color.Black,
+                textAlign = TextAlign.Center,
+                softWrap = true
+            )
+            Spacer(Modifier.fillMaxWidth(1f).height(82.dp))
+            VerticalSliderForm(
+                answerList = listOf("매우 동의해요", "조금 동의해요", "보통이에요", "조금 반대해요", "매우 반대해요"),
+                onValueChange = { index -> selectedIndex = index})
+        }
+        LoopyFilledButton(
+            "작성 완료",
+            onClick = { onClickFinishButton(selectedIndex) },
+            modifier = Modifier.fillMaxWidth(1f)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun SliderRoundPreview() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize().background(color = ColorPalette.Neautral0)
+    ) { paddingValue ->
+        Column(
+            Modifier.fillMaxSize(1f).padding(paddingValue).background(color = ColorPalette.Neautral0),
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+            LoopyTopBar(
+                title = "오늘의 질문",
+                icon = painterResource(R.drawable.ic_back),
+                onClickIcon = {})
+            SliderContent(onClickFinishButton = {roundIndex ->  })
+        }
+    }
+}
