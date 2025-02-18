0f3023e8b3eaaceedb492e8e7b02cb5952fbfe36
이용인 <yongin0517@gmail.com>
2025-02-18 22:00:41 +0900

    Feat: 캐릭터 분석 결과 조회 추가

--- app/src/main/java/com/jaknaeso/app/data/service/CharacterService.kt
+++ app/src/main/java/com/jaknaeso/app/data/service/CharacterService.kt
@@ -2,7 +2,7 @@ package com.jaknaeso.app.data.service
 
 import com.jaknaeso.app.data.entity.LoopyResult
 import com.jaknaeso.app.data.entity.response.CharacterDetailResponse
-import com.jaknaeso.app.data.entity.response.CharacterReportResponse
+import com.jaknaeso.app.data.entity.response.CharacterGraphValueResponse
 import com.jaknaeso.app.data.entity.response.Characters
 import com.skydoves.sandwich.ApiResponse
 import retrofit2.http.GET
@@ -16,9 +16,15 @@ interface CharacterService {
     @GET("/api/v1/characters/latest")
     suspend fun getLatestCharacter(@Query("memberId") memberId: String): ApiResponse<LoopyResult<CharacterDetailResponse>>
 
-    @GET("/api/v1/characters/{characterId}/report")
+    @GET("/api/v1/characters/{characterId}")
     suspend fun getCharacterReport(
         @Path("characterId") characterId: String,
         @Query("memberId") memberId: String
-    ): ApiResponse<LoopyResult<CharacterReportResponse>>
+    ): ApiResponse<LoopyResult<CharacterDetailResponse>>
+
+    @GET("/api/v1/characters/{characterId}/report")
+    suspend fun getCharacterGraphValue(
+        @Path("characterId") characterId: String,
+        @Query("memberId") memberId: String
+    ): ApiResponse<LoopyResult<CharacterGraphValueResponse>>
 }
