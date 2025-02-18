f95acaee97b3e76fbcf646a82b0614672b466f2b
이용인 <yongin0517@gmail.com>
2025-02-19 03:26:52 +0900

    Delete: 로그인 화면으로 이전된 로그인 유지 로직 삭제

--- app/src/main/java/com/jaknaeso/app/presentation/viewmodel/MainViewmodel.kt
+++ app/src/main/java/com/jaknaeso/app/presentation/viewmodel/MainViewmodel.kt
@@ -1,35 +1,19 @@
 package com.jaknaeso.app.presentation.viewmodel
 
-import androidx.lifecycle.viewModelScope
-import com.jaknaeso.app.domain.usecase.CheckLoginedUserUseCase
 import com.jaknaeso.app.presentation.contract.MainEffect
 import com.jaknaeso.app.presentation.contract.MainEvent
 import com.jaknaeso.app.presentation.contract.MainState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewmodel @Inject constructor(private val checkLoginedUserUseCase: CheckLoginedUserUseCase) :
+class MainViewmodel @Inject constructor() :
     BaseViewModel<MainEvent, MainState, MainEffect>() {
-
-    init {
-        branchInitialRoute()
-    }
-
     override fun createInitialState(): MainState {
         return MainState
     }
 
     override fun handleEvent(event: MainEvent) {
-    }
-
-    fun branchInitialRoute() {
-        viewModelScope.launch {
-            if (!checkLoginedUserUseCase.isLoginedUser()) {
-                setEffect(MainEffect.NavigateToLogin)
-            }
-        }
     }
 
 }
