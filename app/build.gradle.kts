7bda129df562419e12669646c5130ab2a772b3ec
이용인 <yongin0517@gmail.com>
2025-02-19 18:21:22 +0900

    Chore: 버전코드 변경(3->4)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.jaknaeso.app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
