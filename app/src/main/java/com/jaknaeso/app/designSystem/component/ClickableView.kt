277aa8848f7e3c6fb4244fac45becb2ca7096f71
이용인 <yongin0517@gmail.com>
2025-02-20 18:42:04 +0900

    Design: ClickableView 라이플 이펙트 제거

--- app/src/main/java/com/jaknaeso/app/designSystem/component/ClickableView.kt
+++ app/src/main/java/com/jaknaeso/app/designSystem/component/ClickableView.kt
@@ -1,12 +1,14 @@
 package com.jaknaeso.app.designSystem.component
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Text
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -21,7 +23,7 @@ fun ClickableText(
     textColor: Color = Color.Black,
     onClick: () -> Unit,
     icon: Painter? = null,
-    iconColor:Color = Color.Black
+    iconColor: Color = Color.Black
 ) {
     Row(
         modifier = Modifier.fillMaxWidth(1f),
@@ -34,7 +36,9 @@ fun ClickableText(
                 painter = icon,
                 contentDescription = null,
                 tint = iconColor,
-                modifier = Modifier.clickable { onClick() })
+                modifier = Modifier.clickable(
+                    indication = null,
+                    interactionSource = remember { MutableInteractionSource() }) { onClick() })
         }
     }
 }
