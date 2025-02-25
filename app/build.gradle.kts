0c35dfa95032931e1aaf449db1b7897cd3d8500e
이용인 <yongin0517@gmail.com>
2025-02-26 04:35:42 +0900

    Chore: 버전 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.jaknaeso.app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.0.0"
+        versionCode = 12
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "LIBRARY_PACKAGE_NAME", "\"com.jaknaeso.app\"")
