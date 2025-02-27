fb9535284773a387ac29ae314456ea1e67e29edc
이용인 <yongin0517@gmail.com>
2025-02-28 02:58:06 +0900

    Fix: 오타 수정

--- app/src/main/java/com/jaknaeso/app/presentation/view/RoundCompleteScreen.kt
+++ app/src/main/java/com/jaknaeso/app/presentation/view/RoundCompleteScreen.kt
@@ -28,7 +28,7 @@ fun RoundCompleteScreen(navigateToHome: () -> Unit, remaingRounds: Int) {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                text = "${15 - remaingRounds}g회차 질문에\n답변을 완료했어요!",
+                text = "${15 - remaingRounds}회차 질문에\n답변을 완료했어요!",
                 style = TextStyles.title02,
                 softWrap = true,
                 textAlign = TextAlign.Center
