39849c735ae2c78b07cf80af13dfdd202de5330e
이용인 <yongin0517@gmail.com>
2025-03-04 00:07:21 +0900

    Fix: 초기 화면 라우팅 기준 값 불러오는 작업 비동기 오류 수정

--- app/src/main/java/com/jaknaeso/app/domain/usecase/CheckLoginedUserUseCase.kt
+++ app/src/main/java/com/jaknaeso/app/domain/usecase/CheckLoginedUserUseCase.kt
@@ -1,19 +1,34 @@
 package com.jaknaeso.app.domain.usecase
 
+import android.util.Log
 import com.jaknaeso.app.domain.model.InitialRoute
 import com.jaknaeso.app.domain.repository.LoginRepository
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import javax.inject.Inject
 
 class CheckLoginedUserUseCase @Inject constructor(private val loginRepository: LoginRepository) {
     suspend operator fun invoke(): InitialRoute {
-        val accessToken = loginRepository.getAccessToken()
-        val refreshToken = loginRepository.getRefreshToken()
-        val isOnBoardingCompleted = loginRepository.getIsOnBoardingCompleted() ?: false
+        val result = coroutineScope {
+            val accessTokenDeferred = async { loginRepository.getAccessToken() }
+            val refreshTokenDeferred = async { loginRepository.getRefreshToken() }
+            val isOnBoardingCompletedDeferred = async { loginRepository.getIsOnBoardingCompleted() ?: false }
+
+            Triple(
+                accessTokenDeferred.await(),
+                refreshTokenDeferred.await(),
+                isOnBoardingCompletedDeferred.await()
+            )
+        }
+
+        val (accessToken, refreshToken, isOnBoardingCompleted) = result
+
         if (accessToken.isNullOrBlank() && refreshToken.isNullOrBlank() && !isOnBoardingCompleted) { //회원가입,온보딩 안됨
             return InitialRoute.Login
         } else if (!accessToken.isNullOrBlank() && !refreshToken.isNullOrBlank() && isOnBoardingCompleted) {//회원가입,온보딩 됨
             return InitialRoute.Home
         } else if (!accessToken.isNullOrBlank() && !refreshToken.isNullOrBlank() && !isOnBoardingCompleted) { //온보딩만 안됨
+            Log.d("CheckLoginedUserUseCase", "isOnBoardingCompleted: ${isOnBoardingCompleted}")
             return InitialRoute.OnBoardingInformation
         } else { //온보딩만 된 경우는 존재하지 않음
             return InitialRoute.Login
