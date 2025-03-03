6476f76e89437c7d2e8c5df26dd06c9debcb6198
이용인 <yongin0517@gmail.com>
2025-03-04 00:28:34 +0900

    Chore: 버그 수정 후 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.jaknaeso.app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 13
-        versionName = "1.0.2"
+        versionCode = 14
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "LIBRARY_PACKAGE_NAME", "\"com.jaknaeso.app\"")
